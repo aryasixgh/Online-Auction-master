743ed35cde87c87b78caa4d0b963c0dc8f06f444
Arya Singh <70582356+aryasixgh@users.noreply.github.com>
2024-10-10 11:53:02 +0530

    Add files via upload

---
+++ online-Auction-master/online-Auction-master/login.php
@@ -0,0 +1,148 @@
+<?php
+    require_once ('connection.php');
+    if(isset($_COOKIE['user_id'])){
+        header('location:index.php',false);
+        die;
+    }
+?>
+
+<html>
+
+<head>
+    <title>Auction - Log In/Sign Up</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Sulphur+Point:wght@400;700&display=swap" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.16.0/css/mdb.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="styles/master.css">
+
+    <style>
+        /* Hide the sign-up section by default */
+        #signup {
+            display: none;
+        }
+    </style>
+    
+    <script>
+        // Function to show the sign-up form
+        function showSignUp() {
+            document.getElementById('signup').style.display = 'block'; // Show the sign-up form
+            document.getElementById('login').style.display = 'none';
+        }
+    </script>
+</head>
+
+<body class="entirePage">
+    <div class="jumbotron"></div>
+    <div class="row justify-content-center">
+        <div class="col-auto"></div>
+        
+        <!-- Log In Section --> 
+        <div class="col col-4" id="login">
+            <center>
+                <h1>Log In</h1>
+                <form action="#" method="post">
+                    <div class="md-form">
+                        <input required type="email" name="Email" placeholder="Email Address">
+                        <br><br>
+                        <input required type="password" name="password" placeholder="Your Password">
+                        <br><br>
+                        <input type="submit" name='login' value="Log In" class="btn btn-primary" style="float: centre;">
+                        <!-- Button to show the Sign Up section -->
+                        <button onclick="showSignUp()" class="btn btn-primary" style="float: centre">New Sign Up Here</button>
+                    </div>
+                </form>
+                <br>
+            
+            </center>
+        </div>
+
+        <!-- <div class="col col-1" style="border-right: 1px solid #7070702F"></div> -->
+
+        <!-- Sign Up Section -->
+        <div class="col col-4" id="signup">
+            <center>
+                <h1>Sign Up</h1>
+            </center>
+            <form action="#" method="post">
+                <center>
+                    <div class="md-form">
+                        <input required type="text" name="FirstName" placeholder="First Name">
+                        <input required type="email" name="email" placeholder="Email Address" style="float: right;">
+                        <br><br>
+                        <input required type="text" name="LastName" placeholder="Last Name">
+                        <input required type="password" name="password" placeholder="Your Password" style="float: right;">
+                        <br><br>
+                        <input required type="number" name="Contact" placeholder="Contact">
+                        <input type="submit" value="Sign Up" class="btn btn-primary" name="signUp" style="float: right; margin-left: 60px;">
+                        <br><br>
+                        <select required name="payment" class="custom-select">
+                            <option value="easypaisa">EasyPaisa</option>
+                            <option value="cashOnDelivery">Cash on Delivery</option>
+                        </select>
+                    </div>
+                </center>
+            </form>
+        </div>
+
+        <div class="col col-1"></div>
+    </div>
+</body>
+
+</html>
+
+<?php
+    // Log In Handler
+    if(isset($_POST['login'])){
+        $email = $_POST['Email'];
+        $password = $_POST['password'];
+        $query = "SELECT * FROM `auction_user` WHERE `user_email` = '$email' AND `user_password` = '$password'";
+        $result = $conn->query($query);
+        $rows = $result->num_rows;
+        if($rows == 1){
+            while ($data = $result->fetch_assoc()){
+                setcookie('user_id', $data['user_id'], time() + 3600); // Logged in for one hour
+                setcookie('user_first_name', $data['user_first_name'], time() + 3600);
+                setcookie('user_last_name', $data['user_last_name'], time() + 3600);
+                header('location:index.php');
+            }
+        }else{
+            echo "<br><br><center>Please Double Check your Credentials</center>";
+        }
+    }
+
+    // Sign Up Handler
+    if(isset($_POST['signUp'])){
+        $user_first_name = $_POST['FirstName'];
+        $user_last_name = $_POST['LastName'];
+        $user_contact = $_POST['Contact'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $payment = $_POST['payment'];
+        $query = "SELECT * FROM `auction_user` WHERE `user_email` = '$email' OR `user_contact` = '$user_contact'";
+        $result = $conn->query($query);
+        $rows = $result->num_rows;
+        if($rows >= 1){
+            print "<br><br><center>There Exists a User with same email or Contact</center>" ;
+        }else{
+            if($payment == 'easypaisa') $payment = 'esp';
+            else $payment = 'cod';
+            $query = "INSERT INTO `auction_user` (`user_first_name`, `user_last_name`, `user_email`, `user_contact`, `user_password`, `payment`) VALUES ('$user_first_name', '$user_last_name', '$email', '$user_contact', '$password', '$payment')";
+            $result = $conn->query($query);
+            if(!$result){
+                echo '<br><br><center>We Encountered a Problem, please try again</center>';
+            }else{
+                $query = "SELECT * FROM `auction_user` WHERE `user_email` = '$email'";
+                $result = $conn->query($query);
+                $rows = $result->num_rows;
+                if($rows == 1){
+                    while ($data = $result->fetch_assoc())
+                        $userID = $data['user_id'];
+                }             
+                setcookie('user_id', $userID, time() + 3600); // Logged in for one hour
+                setcookie('user_first_name', $user_first_name, time() + 3600);
+                setcookie('user_last_name', $user_last_name, time() + 3600);
+                header('location:index.php');
+            }
+        }
+    }
+?>
---
+++ online-Auction-master/online-Auction-master/navigation.php
@@ -0,0 +1,81 @@
+<?php
+    if(!isset($_COOKIE['user_id'])){
+        header('location:login.php',false);
+        die;
+    }
+    if(isset($_POST['signOut'])){
+        setcookie('user_id',$_COOKIE['user_id'],time() - 1);
+        header('location:login.php');
+    }
+    if(isset($_POST['addItem'])){
+        header('location:posting_item.php');
+    }
+    if(isset($_POST['search'])){
+        $itemName=$_POST['itemName'];
+        header("location:items_search.php?itemName=$itemName");
+        // 
+    }
+?>
+
+<html>
+    <body>
+        <div class="jumbotron">
+            <div class="row">
+                <div class="col col-3">
+                    <h1 style='float:left;'><a class="text-primary" href="index.php">Home</a></h1>
+                </div>
+                <div class="col col-6">
+                <form method='POST'>
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <div class="input-group-text">
+                                <img src="assets/search-solid.svg" alt="Search Icon" height='15' width='15'>
+                            </div>
+                        </div>
+                        <input type="text" name="itemName" required placeholder='Search an Item' class='form-control'>
+                        <input type="submit" value="Search" name='search' class='btn btn-outline-primary' style='float:right;'>
+                    </div>
+                </form>
+                </div>
+                <div class="col col-3">
+                    <div style = "float:right;">
+                        <h5>Hello, <?php echo"{$_COOKIE['user_first_name']}"; ?></h5>
+                        <form method='POST'>
+                            <input type="submit" value="+" class="btn btn-outline-success ml-2" name='addItem'>
+                            <input type="submit" value="Sign Out" class="btn btn-outline-danger" name='signOut'>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div style='float:right;'>
+                <form action="#" method="post">
+                    <input type="submit" value='Sort By Price' name='price' class="btn btn-primary">
+                    <input type="submit" value='Sort By Categories' name='category' class="btn btn-primary">
+                </form>
+                <?php
+                    if(isset($_POST['price'])){
+                        header('location:sortedbyprice.php');
+                    }if(isset($_POST['category'])){
+                        header('location:sortedbycategories.php');
+                    }
+                ?>
+            </div>
+            <div style='float:left;'>
+                <form action="#" method="post">
+                    <input type="submit" value='About Us' name='about' class="btn btn-primary">
+                    <input type="submit" value='Contact Us' name='contact' class="btn btn-primary">
+                    <input type="submit" value='Privacy Policy' name='privpol' class="btn btn-primary">
+                </form>
+                <?php
+                    if(isset($_POST['about'])){
+                        header('location:about.php');
+                    }if(isset($_POST['privpol'])){
+                        header('location:privacy.php');
+                    }if(isset($_POST['contact'])){
+                        header('location:contact.php');
+                    }
+                ?>
+            </div>
+        </div>
+    </body>
+</html>
